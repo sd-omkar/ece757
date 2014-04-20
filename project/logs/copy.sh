435b90c7973064f2309569dd55eab51310c7f81c
sd-omkar <sd.omkar@gmail.com>
2014-04-20 00:39:10 -0500

    logs

---
+++ project/logs/copy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash 
+
+cp /scratch/omkar/rtm/bayes/run_log/* ./rtm/bayes/
+cp /scratch/omkar/rtm/genome//run_log/* ./rtm/genome//
+cp /scratch/omkar/rtm/intruder//run_log/* ./rtm/intruder//
+cp /scratch/omkar/rtm/kmeans//run_log/* ./rtm/kmeans//
+cp /scratch/omkar/rtm/labyrinth//run_log/* ./rtm/labyrinth//
+cp /scratch/omkar/rtm/ssca2//run_log/* ./rtm/ssca2//
+cp /scratch/omkar/rtm/vacation//run_log/* ./rtm/vacation//
+cp /scratch/omkar/rtm/yada//run_log/* ./rtm/yada//
